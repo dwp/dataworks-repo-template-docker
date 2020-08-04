b9a99d7b2fccffe708650fd0c35d241b36bb235c
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-08-04 11:37:51 +0100

    adding TF and concourse pipelines for ecr mirroring
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

---
+++ ecr.tf
@@ -0,0 +1,16 @@
+resource "aws_ecr_repository" "example" {
+  name = "hive-exporter"
+  tags = merge(
+    local.common_tags,
+    { DockerHub : "dwpdigital/hive-exporter" }
+  )
+}
+
+resource "aws_ecr_repository_policy" "example" {
+  repository = aws_ecr_repository.example.name
+  policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
+}
+
+output "ecr_example_url" {
+  value = aws_ecr_repository.example.repository_url
+}
